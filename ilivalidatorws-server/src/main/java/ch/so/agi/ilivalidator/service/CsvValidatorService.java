222aa880487b93b4f93057c29f6209f13fff0d98
edigonzales <edi.gonzales@gmail.com>
2023-09-29 10:35:00 +0200

    fix csvvalidator

--- ilivalidatorws-server/src/main/java/ch/so/agi/ilivalidator/service/CsvValidatorService.java
+++ ilivalidatorws-server/src/main/java/ch/so/agi/ilivalidator/service/CsvValidatorService.java
@@ -87,7 +87,10 @@ public class CsvValidatorService {
                 configFileNames.add(localCopy.toFile().getAbsolutePath());
             }
         }
-        logFile = Paths.get(new File(csvFileNames.get(0)).getParent(), jobDirectoryPath.toString() + ".log");
+        // Weil das Verzeichnis nicht nur aus der JobId besteht, sondern ein Prefix besitzt,
+        // muss man dieses wieder entfernen, um den Logfile-Namen zu erhalten (der nur aus JobId)
+        // bestehen soll.
+        logFile = Paths.get(new File(csvFileNames.get(0)).getParent(), jobDirectoryPath.toString().substring(folderPrefix.length()) + ".log");
         logFileName = logFile.toFile().getAbsolutePath();                
     
         Settings settings = new Settings();
